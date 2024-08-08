8d52bdc3b32cba36c24b1f0bfbb7d6e4ab4839f1
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-08-08 18:29:45 +0400

    modified styles for todos

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
 
 @Component({
   selector: "app-side-nav",
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatExpansionModule],
   templateUrl: "./side-nav.component.html",
   styleUrl: "./side-nav.component.scss",
 })
