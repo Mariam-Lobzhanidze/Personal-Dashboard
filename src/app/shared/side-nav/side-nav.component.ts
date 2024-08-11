b9ce8c079fff7f39d6c88225fa26d00ecd2d681e
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-08-11 23:37:36 +0400

    added categories chips in todolist section to change category  directly in list

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -4,15 +4,13 @@ import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { TodoService } from "../../services/todo.service";
-import { CommonModule } from "@angular/common";
-
 import { UnsubscribeComponent } from "../unsubscribeComponent";
 import { takeUntil } from "rxjs";
 
 @Component({
   selector: "app-side-nav",
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatListModule, MatIconModule, MatProgressBarModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatProgressBarModule],
   templateUrl: "./side-nav.component.html",
   styleUrl: "./side-nav.component.scss",
 })
@@ -24,24 +22,20 @@ export class SideNavComponent extends UnsubscribeComponent implements OnInit {
   }
 
   public ngOnInit(): void {
+    /* */
     this.todoService.toDoList$.pipe(takeUntil(this.destroy$)).subscribe((todos) => {
       const uniqueCategoryNames = [...new Set(todos.map((todo) => todo.categoryName))];
-
       this.toDosByCategory = uniqueCategoryNames.map((name) => {
         const itemsInCategory = name === "all" ? todos : todos.filter((todo) => todo.categoryName === name);
-
-        // Calculate the completion rate
+        //
         const totalItems = itemsInCategory.length;
         const completedItems = itemsInCategory.filter((todo) => todo.completed).length;
         const completionRate = totalItems > 0 ? Math.round((completedItems / totalItems) * 100) : 0;
-
         return {
           categoryName: name,
-          // items: itemsInCategory,
           completionRate,
         };
       });
-
       console.log(this.toDosByCategory);
     });
   }
--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,62 +1,71 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { TodoListItemComponent } from "./todo-list-item/todo-list-item.component";
 import { TodoService } from "../../services/todo.service";
 import { Todo } from "../../interfaces/todo.interface";
-import { map, takeUntil } from "rxjs";
+import { map, Observable, switchMap, takeUntil, tap } from "rxjs";
 import { AddNewTodoComponent } from "./add-new-todo/add-new-todo.component";
 import { UnsubscribeComponent } from "../../shared/unsubscribeComponent";
-import { MatDialog } from "@angular/material/dialog";
+// import { MatDialog } from "@angular/material/dialog";
 import { SideNavComponent } from "../../shared/side-nav/side-nav.component";
+import { MatChipsModule } from "@angular/material/chips";
+import { Category } from "../../interfaces/category.interface";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-todo-list",
   standalone: true,
-  imports: [TodoListItemComponent, AddNewTodoComponent, SideNavComponent],
+  imports: [TodoListItemComponent, AddNewTodoComponent, SideNavComponent, MatChipsModule, CommonModule],
   templateUrl: "./todo-list.component.html",
   styleUrl: "./todo-list.component.scss",
 })
 export class TodoListComponent extends UnsubscribeComponent implements OnInit {
   public activeCategory?: string | null | undefined;
+  public categories$?: Observable<Category[]>;
 
   public todos?: Todo[];
 
   public constructor(
     private route: ActivatedRoute,
     private todoService: TodoService,
-    private dialog: MatDialog
+    // private dialog: MatDialog,
+    private router: Router
   ) {
     super();
   }
 
   public ngOnInit(): void {
-    this.getActiveToDoCategory();
-    this.getToDoList();
-  }
+    this.categories$ = this.todoService.categories$;
 
-  private getToDoList(): void {
-    this.todoService.toDoList$
+    this.route.queryParamMap
       .pipe(
         takeUntil(this.destroy$),
-        map((todos) =>
-          this.activeCategory === "all"
-            ? todos
-            : todos.filter((todo) => todo.categoryName === this.activeCategory)
+        tap((paramMap) => {
+          this.activeCategory = paramMap.get("category") || "all";
+        }),
+        switchMap(() =>
+          this.todoService.toDoList$.pipe(
+            map((todos) =>
+              this.activeCategory === "all"
+                ? todos
+                : todos.filter((todo) => todo.categoryName === this.activeCategory)
+            )
+          )
         )
       )
       .subscribe((toDos) => {
         this.todos = toDos;
-        // console.log(this.todos);
       });
-  }
-
-  private getActiveToDoCategory(): void {
-    this.route.queryParamMap.subscribe((paramMap) => {
-      this.activeCategory = paramMap.get("category");
-    });
   }
 
   public onOpenAddToDoDialog(): void {
     this.todoService.openDialog(AddNewTodoComponent);
+  }
+
+  public onCategorySelect(title: string): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { category: title },
+    });
   }
 }
