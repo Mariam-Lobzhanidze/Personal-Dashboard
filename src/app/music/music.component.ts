d5d12451f684f3ebff2c3091ef98aad2c41f253b
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-08-31 01:29:44 +0400

    added search functionality and corrected music page styles

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,33 +1,50 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { MusicService } from "./music.Service";
+import { TruncatePipe } from "../pipes/truncate.pipe";
+import { FormsModule } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-music",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TruncatePipe, FormsModule],
   templateUrl: "./music.component.html",
   styleUrls: ["./music.component.scss"],
 })
 export class MusicComponent implements OnInit {
+  public searchQuery: string = "madonna";
   public musicVideos: any[] = [];
-  public popularMusicVideos: any[] = [];
   public selectedVideoUrl: string = "";
 
-  constructor(private musicService: MusicService, private sanitizer: DomSanitizer) {}
+  constructor(
+    private musicService: MusicService,
+    private sanitizer: DomSanitizer,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   public ngOnInit(): void {
-    this.getPopularMusic();
-    this.searchVideos("rihanna");
+    this.route.queryParams.subscribe((params) => {
+      const query = params["query"];
+      if (query) {
+        this.searchQuery = query;
+        this.searchVideos();
+      }
+    });
+    // this.searchVideos(this.searchQuery as string);
   }
 
-  public getPopularMusic(): void {
-    this.musicService.getMostPopularMusic("US", "10").subscribe((response) => {
-      this.popularMusicVideos = response.items;
-      console.log(this.popularMusicVideos);
-    });
+  public onSearchMusic(): void {
+    if (this.searchQuery) {
+      this.router.navigate([], {
+        queryParams: { query: this.searchQuery },
+        queryParamsHandling: "merge",
+      });
+
+      this.searchVideos();
+    }
   }
 
   public sanitize(url: string): SafeResourceUrl {
@@ -38,8 +55,8 @@ export class MusicComponent implements OnInit {
     this.selectedVideoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
   }
 
-  public searchVideos(query: string): void {
-    this.musicService.searchVideos(query).subscribe(
+  public searchVideos(): void {
+    this.musicService.searchVideos(this.searchQuery).subscribe(
       (response) => {
         this.musicVideos = response.items.map((item: any) => {
           return {
